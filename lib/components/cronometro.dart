9f9d4796bf5a04dd69868befd7b51eff3de908ad
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-12-27 16:01:19 -0300

    button cronometro style

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pomodor_tech_work/components/cronometro_botao.dart';
 
 class Cronometro extends StatelessWidget {
   const Cronometro({Key? key}) : super(key: key);
@@ -14,7 +15,16 @@ class Cronometro extends StatelessWidget {
           Text("Hora de Trabalhar",
               style: TextStyle(fontSize: 40, color: Colors.white)),
           SizedBox(height: 20),
-          Text("25:00", style: TextStyle(fontSize: 120, color: Colors.white))
+          Text("25:00", style: TextStyle(fontSize: 120, color: Colors.white)),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CronometroBotao(texto: 'Iniciar', icone: Icons.play_arrow),
+              // CronometroBotao(texto: 'Parar', icone: Icons.stop),
+              CronometroBotao(texto: 'Reiniciar', icone: Icons.refresh),
+            ],
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() {
   runApp(const MyApp());
 }
 
+
+//TODO: Componente Botão Cronometro
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -15,15 +18,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const Pomodoro(),
